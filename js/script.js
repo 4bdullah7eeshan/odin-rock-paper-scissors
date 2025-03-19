88e9f955595440140ab81621e3340944d44fb1af
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2025-03-19 21:52:41 +0530

    refactor: reorganize functions
    Placed dependent functions on top.

--- js/script.js
+++ js/script.js
@@ -4,6 +4,54 @@ let humanScore,
 humanScore = 0;
 computerScore = 0;
 
+function getRandomNumber(max) {
+    // This function takes a `max` number and returns a number from `0` to `max - 1` randomly
+
+    let randomDecimalBetweenZeroAndOne,
+        randomRationalNumberBetweenZeroAndMax,
+        randomIntegerBetweenZeroAndMax;
+    
+    // S1: Get a random decimal number between 0(inclusive) and 1.
+    randomDecimalBetweenZeroAndOne = Math.random(); // 0.57
+    
+    // S2: Get a random rational number between 0(inclusive) and max.
+    randomRationalNumberBetweenZeroAndMax = randomDecimalBetweenZeroAndOne * max; // 0.57 * 2 = 1.14
+
+    // s3: Get a random integer between 0(inclusive) and max.
+    randomIntegerBetweenZeroAndMax = Math.floor(randomRationalNumberBetweenZeroAndMax); // 1
+
+    return randomIntegerBetweenZeroAndMax;
+}
+
+function getComputerChoice() {
+    let randomNumber,
+        computerChoice;
+
+    randomNumber = getRandomNumber(3);
+
+    if (randomNumber === 0) {
+        computerChoice = "rock";
+    } else if (randomNumber === 1) {
+        computerChoice = "paper";
+    } else {
+        computerChoice = "scissors";
+    }
+
+    return computerChoice;
+}
+
+function getHumanChoice() {
+    let promptMessage,
+        humanChoice;
+
+    promptMessage = "Make a move! Rock? Paper? or Scissors?";
+
+    humanChoice = prompt(promptMessage);
+
+    return humanChoice;
+}
+
+
 function getRoundResult(moveOne, moveTwo) {
     let roundResult;
 
@@ -59,51 +107,4 @@ function playGame() {
         console.log("You lost the game!");
     }
     
-}
-
-function getRandomNumber(max) {
-    // This function takes a `max` number and returns a number from `0` to `max - 1` randomly
-
-    let randomDecimalBetweenZeroAndOne,
-        randomRationalNumberBetweenZeroAndMax,
-        randomIntegerBetweenZeroAndMax;
-    
-    // S1: Get a random decimal number between 0(inclusive) and 1.
-    randomDecimalBetweenZeroAndOne = Math.random(); // 0.57
-    
-    // S2: Get a random rational number between 0(inclusive) and max.
-    randomRationalNumberBetweenZeroAndMax = randomDecimalBetweenZeroAndOne * max; // 0.57 * 2 = 1.14
-
-    // s3: Get a random integer between 0(inclusive) and max.
-    randomIntegerBetweenZeroAndMax = Math.floor(randomRationalNumberBetweenZeroAndMax); // 1
-
-    return randomIntegerBetweenZeroAndMax;
-}
-
-function getComputerChoice() {
-    let randomNumber,
-        computerChoice;
-
-    randomNumber = getRandomNumber(3);
-
-    if (randomNumber === 0) {
-        computerChoice = "rock";
-    } else if (randomNumber === 1) {
-        computerChoice = "paper";
-    } else {
-        computerChoice = "scissors";
-    }
-
-    return computerChoice;
-}
-
-function getHumanChoice() {
-    let promptMessage,
-        humanChoice;
-
-    promptMessage = "Make a move! Rock? Paper? or Scissors?";
-
-    humanChoice = prompt(promptMessage);
-
-    return humanChoice;
 }
