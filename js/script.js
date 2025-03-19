85217c0c21d9a56cbb878502df595d86d0cfcf50
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2025-03-19 16:17:16 +0530

    docs: add comments

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
 function getRandomNumber(max) {
+    // This function takes a `max` number and returns a number from `0` to `max - 1` randomly
+
     let randomDecimalBetweenZeroAndOne,
         randomRationalNumberBetweenZeroAndMax,
         randomIntegerBetweenZeroAndMax;
     
-    randomDecimalBetweenZeroAndOne = Math.random();
-    randomRationalNumberBetweenZeroAndMax = randomDecimalBetweenZeroAndOne * max;
-    randomIntegerBetweenZeroAndMax = Math.floor(randomRationalNumberBetweenZeroAndMax);
+    // S1: Get a random decimal number between 0(inclusive) and 1.
+    randomDecimalBetweenZeroAndOne = Math.random(); // 0.57
+    
+    // S2: Get a random rational number between 0(inclusive) and max.
+    randomRationalNumberBetweenZeroAndMax = randomDecimalBetweenZeroAndOne * max; // 0.57 * 2 = 1.14
+
+    // s3: Get a random integer between 0(inclusive) and max.
+    randomIntegerBetweenZeroAndMax = Math.floor(randomRationalNumberBetweenZeroAndMax); // 1
 
     return randomIntegerBetweenZeroAndMax;
 }
